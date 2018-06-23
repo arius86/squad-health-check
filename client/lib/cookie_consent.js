3bf6005676c19f0d41bbc4791575c57e3e4462e7
Arius <arkadiusz.ostrycharz@gmail.com>
2018-06-23 21:00:51 +0200

    cookie consent

---
+++ client/lib/cookie_consent.js
@@ -0,0 +1,12 @@
+var options = {
+  cookieTitle: "This site use cookies",
+  cookieMessage: "SquadHealthCheck.info uses cookies only for page analytics."
+     + " We hope you are fine with that :)",
+  showLink: false,
+  acceptButtonText: "Accept and Continue",
+  html: false,
+  expirationInDays: 30,
+  forceShow: false
+};
+
+CookieConsent.init(options);
